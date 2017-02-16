969720a1136a83a991469c102948dd740c3f7631
André Pereira <ap080221063@gmail.com>
2017-02-16 15:52:35 +0000

    changes to start using an array as a stack instead of multiple variables

--- scripts/app.js
+++ scripts/app.js
@@ -1,8 +1,8 @@
 angular.module('App', []).controller('Ctrl', function($scope) {
     $scope.numbRows = [1,2,3,4,5];
     $scope.buttonArray = [
-                          {row:1, char: '-' ,optionalClass:''},
-                          {row:1, char: '-' ,optionalClass:''},		
+                          {row:1, char: '-' ,optionalClass:'hide'},
+                          {row:1, char: '-' ,optionalClass:'hide'},		
 						  {row:1, char: 'CE',optionalClass:'btn-primary cellSmallerFontSize'},
                           {row:1, char: 'C' ,optionalClass:'btn-primary'},                          						  
                           {row:2, char: 7   ,optionalClass:''},
@@ -22,94 +22,124 @@ angular.module('App', []).controller('Ctrl', function($scope) {
                           {row:5, char: '/' ,optionalClass:''},
                           {row:5, char: '=' ,optionalClass:'btn-success'},						  
     ];
-    $scope.panelNumber = 0;
-    $scope.memoryNumber = '';
-    $scope.memoryOperator = '';
-	$scope.equalOperatorApplied = false;
+	$scope.memory = [0];
+	$scope.maxMemory = 10;	
 	
-    $scope.writeToPanel = function(invar){
-        if(isNaN(invar)){//its an operator
-            console.log(invar);
-            if(invar == '='){
-                if($scope.memoryOperator!=''){
-                    $scope.memoryNumber = isNaN($scope.memoryNumber)?0:$scope.memoryNumber;
-					$scope.equalOperatorApplied = true;
-                    switch($scope.memoryOperator){
-                        case '+':
-                            var aux = $scope.panelNumber;
-                            $scope.panelNumber = Number($scope.memoryNumber) + Number(aux);
-                            $scope.memoryNumber = '';
-                            break;
-                        case '-':
-                            var aux = $scope.panelNumber;
-                            $scope.panelNumber = Number($scope.memoryNumber) - Number(aux);
-                            $scope.memoryNumber = '';
-                            break;
-                        case '*':
-                            var aux = $scope.panelNumber;
-                            $scope.panelNumber = Number($scope.memoryNumber) * Number(aux);
-                            $scope.memoryNumber = '';
-                            break;
-                        case '/':
-                            var aux = $scope.panelNumber;
-                            $scope.panelNumber = Number($scope.memoryNumber) / Number(aux);
-                            $scope.memoryNumber = '';
-                            break;
-                    }
-
-                    $scope.memoryOperator = '';
-                }else{
-                    $scope.memoryOperator = invar;
-                }
-            }else{
-                $scope.memoryOperator = invar;
-            }
-        }else{//its a number
-            console.log(invar);
-				if($scope.equalOperatorApplied){
-					$scope.equalOperatorApplied=false;
-					$scope.memoryNumber = $scope.panelNumber;
-					$scope.panelNumber = invar;
-				}else{			
-					if($scope.panelNumber.length = 1 && $scope.panelNumber == "0"){
-						$scope.panelNumber = invar;
-					}else{
-						if($scope.memoryOperator != ''){
-							if(isNaN($scope.memoryNumber) || $scope.memoryNumber == ''){
-								$scope.memoryNumber = $scope.panelNumber;
-								$scope.panelNumber = invar;
-							}else{
-								$scope.panelNumber = $scope.panelNumber + '' + invar;
-							}
-						}else{
-							$scope.panelNumber = $scope.panelNumber + '' + invar;
-						}
+	$scope.pushInput = function(invar){
+		if($scope.memory.length > $scope.maxMemory){
+			$scope.memory.shift();
+			$scope.memory.push(invar);
+		}else{
+			$scope.memory.push(invar);
+		}
+	}
+	
+	$scope.get = function(index){
+		return $scope.memory[index];
+	}
+	
+	$scope.isNumber = function(invar){
+		if(isNaN(invar)){
+			if(invar == '.'){
+				return true;
+			}else{
+				return false;
+			}
+		}else{
+			return true;
+		}
+	}
+	
+    $scope.pressButton = function(invar){
+		//$scope.pushInput(invar);
+		if(invar == 'C'){
+			$scope.memory = [0];
+			return;
+		}
+		if(invar == 'CE'){
+			$scope.memory.pop();
+			return;
+		}
+		
+		if($scope.memory.length == 0)
+		{
+			if(!$scope.isNumber(invar)){
+				if(invar!='='){
+					$scope.pushInput(0);
+					$scope.pushInput(invar);
+				}
+			}else{
+				$scope.pushInput(invar);	
+			}			
+		}
+		else if($scope.memory.length == 1)
+		{
+			if(!$scope.isNumber(invar)){
+				if(!$scope.isNumber($scope.get(0))){ //cannot accumulate 2 operators
+					$scope.memory[0] = invar;
+				}else{
+					if(invar!='='){
+						$scope.pushInput(invar);
 					}
 				}
-        }
+			}else{
+				if($scope.isNumber($scope.get(0))){ //if it is a number
+					$scope.memory[0] = $scope.memory[0] + '' + invar;
+				}else{
+					$scope.pushInput(invar);
+				}
+			}				
+		}
+		else if($scope.memory.length == 2)
+		{
+			if(!$scope.isNumber(invar)){
+				if(!$scope.isNumber($scope.get(1))){ //cannot accumulate 2 operators
+					$scope.memory[1] = invar;
+				}
+			}else{
+				if($scope.isNumber($scope.get(1))){ //if it is a number
+					$scope.memory[1] = $scope.memory[1] + '' + invar;
+				}else{
+					$scope.pushInput(invar);
+				}
+			}
+		}
+		else
+		{
+					
+			if($scope.isNumber($scope.get($scope.memory.length - 1)) && $scope.isNumber(invar)){
+				$scope.memory[$scope.memory.length - 1] =  $scope.memory[$scope.memory.length - 1] + '' + invar;
+			}else{
+				$scope.pushInput(invar);
+			}
+			
+			if($scope.memory.length>=3){
+			
+				var last         = $scope.get($scope.memory.length - 1);
+				var penultimate  = $scope.get($scope.memory.length - 2);
+				var penultimate2 = $scope.get($scope.memory.length - 3);
+				var penultimate3 = $scope.get($scope.memory.length - 4);
+					
+				if(!$scope.isNumber(last)){//its an operator
+					if(last == '='){
+						switch(penultimate2){
+							case '+':
+								$scope.pushInput(Number(penultimate3) + Number(penultimate));
+								break;
+							case '-':
+								$scope.pushInput(Number(penultimate3) - Number(penultimate));
+								break;
+							case '*':
+								$scope.pushInput(Number(penultimate3) * Number(penultimate));
+								break;
+							case '/':
+								$scope.pushInput(Number(penultimate3) / Number(penultimate));
+								break;
+						}
+					}			
+				}
+			}
+		}
     }
-});
 
-/*
-            if($scope.operator!=''){
-                switch($scope.operator){
-                    case '+':
-                        var aux = $scope.panelNumber;
-                        $scope.panelNumber = Number(aux) + Number(invar);
-                        break;
-                    case '-':
-                        var aux = $scope.panelNumber;
-                        $scope.panelNumber = Number(aux) - Number(invar);
-                        break;
-                    case '*':
-                        var aux = $scope.panelNumber;
-                        $scope.panelNumber = Number(aux) * Number(invar);
-                        break;
-                    case '/':
-                        var aux = $scope.panelNumber;
-                        $scope.panelNumber = Number(aux) / Number(invar);
-                        break;
-                }
-                $scope.operator = '';
-            }
-*/
+});

b5776f9c4745a05fc9b4cf9e13a7d067c73a08f6
André Pereira <ap080221063@gmail.com>
2018-02-09 12:39:32 +0000

    cannot successfully seperate table from search components and mke the work together so they were merged

--- scripts/components.js
+++ scripts/components.js
@@ -1,54 +1,57 @@
 angular.module("components", [])
-    .component("ngtable",{
-      template: "<table class="+"table table-dark"+">" +
+
+
+  .component("ngtable",{
+      template:
+                  "<form>" +
+                  "<div class="+'form-group'+">" +
+                  "<label for="+'lookup'+">search..</label>" +
+                  "<input type="+'"text"'+" class="+'"form-control"'+" id="+'"lookup"'+" aria-describedby="+'"lookupHelp"'+" placeholder="+'"search"'+" ng-model="+"fetchValue"+" ng-change="+"gofetch()"+" />" +
+                  "</div>" +
+                  "</form>" +
+                  "<table class="+"table table-dark"+">" +
                   "<thead><tr>" +
                   "<th scope="+"col"+">Make</th>"+
                   "<th scope="+"col"+">Model</th>"+
                   "</tr></thead>"+
-                  "<tr ng-repeat='x in $ctrl.datasource track by $index'>"+
+                  "<tr ng-repeat='x in datasource track by $index '>"+
                     "<td>{{x.make}}</td>"+
                     "<td>{{x.model}}</td>"+
                   "</tr>"+
                 "</table>",
       controller : function($scope,ngservice){
-            this.datasource = ngservice.data();
+            $scope.datasource = ngservice.getData();
+            $scope.gofetch = function(){
+                                $scope.datasource = ngservice.setFilter(ngservice.getData(),$scope.fetchValue);
+                             };
+            $scope.fetchValue = "";
         },
   })
-  .component("nglookup",{
-      template: "<form>" +
-                "<div class="+'form-group'+">" +
-                "<label for="+'lookup'+">search..</label>" +
-                "<input type="+'"text"'+" class="+'"form-control"'+" id="+'"lookup"'+" aria-describedby="+'"lookupHelp"'+" placeholder="+'"search"'+" ng-model="+"fetchValue"+" ng-change="+"gofetch()"+" />" +
-                //"<input type="+'"text"'+" ng-model="+'"fetchValue"'+" readonly/>" +
-                "</div>" +
-                "</form>",
-      controller: function($scope,ngservice){
-          $scope.gofetch = function(){
-              ngservice.filter(ngservice.data(),$scope.fetchValue);
-              //https://stackoverflow.com/questions/23720988/how-to-filter-json-data-in-javascript-or-jquery
-              //console.log(ngservice.data());
-          };
-          $scope.fetchValue = "";
-      },
 
-  })
   .service('ngservice', function() {
-    this.data = function(){
-        return [
-                 { "make":"Ford", "model": "Mustang"  },
-                 { "make":"BMW",  "model": "320"  },
-                 { "make":"Fiat", "model": "Stilo"  },
-                 { "make":"Ferrari", "model": "Modena" }
-                ]
+
+    this.data = [
+                 { "make":"Ford"   ,  "model": "Mustang"  },
+                 { "make":"BMW"    ,  "model": "320"      },
+                 { "make":"Fiat"   ,  "model": "Stilo"    },
+                 { "make":"Ferrari",  "model": "Modena"   },
+                 { "make":"Seat"   ,  "model": "Ibiza"    },
+                ];
+
+    this.getData = function(){
+        return this.data;
     };
 
-    this.filter = function(ds,f) {
-        var fx = '{"make":"'+f+'"}"';
-        return ds.filter(function(obj) {
-            return Object.keys(fx).every(function(c) {
-                return obj[c] == fx[c];
-            });
-        });
+    this.setFilter = function(ds,f) {
+        if(f.length == 0){
+            return ds;
+        }else{
+            var fx = JSON.parse('{"make":"'+f+'"}');
+            return _.filter(ds, function(input){
+                                      return input.make.indexOf(fx.make) !== -1;
+                                }
+                           );
+        }
     };
 
 });

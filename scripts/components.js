a8581ed57d0d141449181a3a3f6473e650bf93a4
ap080221063 <ap080221063@gmail.com>
2018-02-10 15:48:45 +0000

    modified search row to add model and make options, added mock json file to read cars from

--- scripts/components.js
+++ scripts/components.js
@@ -2,23 +2,40 @@ angular.module("components", [])
 
 
   .component("ngtable",{
-      template:
-                  "<form>" +
-                  "<div class="+'form-group'+">" +
-                  "<label for="+'lookup'+">search..</label>" +
-                  "<input type="+'"text"'+" class="+'"form-control"'+" id="+'"lookup"'+" aria-describedby="+'"lookupHelp"'+" placeholder="+'"search"'+" ng-model="+"fetchValue"+" ng-change="+"gofetch()"+" />" +
-                  "</div>" +
-                  "</form>" +
-                  "<table class="+"table table-dark"+">" +
-                  "<thead><tr>" +
-                  "<th scope="+"col"+">Make</th>"+
-                  "<th scope="+"col"+">Model</th>"+
-                  "</tr></thead>"+
-                  "<tr ng-repeat='x in datasource track by $index '>"+
-                    "<td>{{x.make}}</td>"+
-                    "<td>{{x.model}}</td>"+
-                  "</tr>"+
-                "</table>",
+      template:   "<div class="+'"container-fluid toppadding"'+">"+
+                    "<div class="+'"row rowpadding"'+">"+
+                        "<div class="+'"col-xs-6"'+">"+
+                            "<div class="+'"input-group mb-3"'+">"+
+                                "<div class="+'"input-group-prepend"'+">"+
+                                    "<span class="+'"input-group-text"'+">search</span>"+
+                                    "</div>"+
+                                    "<input type="+'"text"'+" class="+'"form-control"'+" id="+'"lookup"'+" aria-describedby="+'"basic-addon1"'+" placeholder="+'"type here.."'+" ng-model="+"fetchValue"+" ng-change="+"gofetch()"+" />" +
+                                    "<div class="+'"input-group-append"'+">"+
+                                        "<div class="+'"input-group-text checkboxadjust"'+">"+
+                                            "<input class="+'"form-check-input"'+" type="+'"checkbox"'+" id="+'"makeCheckbox"'+">"+
+                                            "<label class="+'"form-check-label"'+" for="+'"makeCheckbox"'+"> Make</label>"+
+                                        "</div>"+
+                                    "</div>"+
+                                    "<div class="+'"input-group-append"'+">"+
+                                        "<div class="+'"input-group-text checkboxadjust"'+">"+
+                                            "<input class="+'"form-check-input"'+" type="+'"checkbox"'+" id="+'"modelCheckbox"'+">"+
+                                            "<label class="+'"form-check-label"'+" for="+'"modelCheckbox"'+"> Model</label>"+
+                                        "</div>"+
+                                    "</div>"+
+                            "</div>"+
+                        "</div>" +
+                    "</div>" +
+                    "<table class="+"table table-dark"+">" +
+                        "<thead><tr>" +
+                        "<th scope="+"col"+">Make</th>"+
+                        "<th scope="+"col"+">Model</th>"+
+                        "</tr></thead>"+
+                        "<tr ng-repeat='x in datasource track by $index '>"+
+                          "<td>{{x.make}}</td>"+
+                          "<td>{{x.model}}</td>"+
+                        "</tr>"+
+                    "</table>"+
+                  "</div>",
       controller : function($scope,ngservice){
             $scope.datasource = ngservice.getData();
             $scope.gofetch = function(){
@@ -55,3 +72,15 @@ angular.module("components", [])
     };
 
 });
+
+
+                            //  "<div class="+'"input-group-append"'+">"+
+                            //      "<form class="+'"form-inline"'+">"+
+                            //          "<div class="+'form-check form-check-inline'+">"+
+                            //              "<input class="+'"form-check-input"'+" type="+'"checkbox"'+" id="+'"makeCheckbox"'+">"+
+                            //              "<label class="+'"form-check-label checkboxadjust"'+" for="+'"makeCheckbox"'+"> Make</label>"+
+                            //              "<input class="+'"form-check-input"'+" type="+'"checkbox"'+" id="+'"modelCheckbox"'+">"+
+                            //              "<label class="+'"form-check-label checkboxadjust"'+" for="+'"modelCheckbox"'+"> Model</label>"+
+                            //          "</div>"+
+                            //      "</form>"+
+                            //  "</div>"+
